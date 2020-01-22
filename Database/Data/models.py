a86e2bc07bb8f92cda043d18a4205c2f845c5beb
Aplefen1 <48839091+Aplefen1@users.noreply.github.com>
2020-01-22 16:28:33 +0000

    Created the first tabe: Child

---
+++ Database/Data/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Child(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    age = models.IntegerField()
+    points = models.IntegerField()
