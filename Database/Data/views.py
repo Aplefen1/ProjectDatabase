68e8471619360a036687d0a45437385db509d28b
Aplefen1 <48839091+Aplefen1@users.noreply.github.com>
2020-01-28 08:50:08 +0000

    Test

--- Database/Data/views.py
+++ Database/Data/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import insertChild
+from django.http import HttpResponse
 
 # Create your views here.
+def insertChild(request):
+	if request.method = "POST":
+		print(request.POST)
