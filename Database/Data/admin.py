49c6f92c96e8b516e57540a07b4b427ea66deaef
Aplefen1 <48839091+Aplefen1@users.noreply.github.com>
2020-02-03 10:29:23 +0000

    Ctreated new models

--- Database/Data/admin.py
+++ Database/Data/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Child
+from .models import *
 # Register your models here.
 admin.site.register(Child)
+admin.site.register(School)
+admin.site.register(Teacher)
+admin.site.register(Task)
+admin.site.register(Classroom)
--- Database/Data/models.py
+++ Database/Data/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from .models import *
 
 # Create your models here.
 class Child(models.Model):
+    child_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     age = models.IntegerField()
     points = models.IntegerField()
+
+    Classroom_id = models.ForeignKey('Classroom', on_delete=models.CASCADE)
+    Teacher_id = models.ForeignKey('Teacher', on_delete=models.CASCADE)
+    School_id = models.ForeignKey('School', on_delete=models.CASCADE)
 
     def __str__(self):
         title = self.first_name + ' ' + self.last_name
@@ -16,15 +22,48 @@ class Child(models.Model):
     def Info(self):
     	return self.first_name, self.last_name, self.username, self.password, self.age, self.points
 
+    def Id(self):
+        return self.child_id
+
 class Teacher(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
+    teacher_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
     age = models.IntegerField()
 
     def __str__(self):
         title = self.first_name + ' ' + self.last_name
         return title
 
+    def Id(self):
+        return self.teacher_id
+
 class Task(models.Model):
+    task_name = models.CharField(max_length=32)
+    task_id = models.AutoField(primary_key=True)
+    task = models.CharField(max_length=32)
+
+    def Id(self):
+        return self.task_id
+
+    def __str__(self):
+        title = self.task_name
+        return title
+
+class Classroom(models.Model):
+    Classroom_id = models.AutoField(primary_key=True)
+    classroom_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        title = self.classroom_name
+        return title
+
+class School(models.Model):
+    School_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        title = self.name
+        return title
