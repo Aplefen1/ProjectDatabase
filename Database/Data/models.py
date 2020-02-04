a5a89a8c825ef9540c314912ba525de01614e720
Aplefen1 <48839091+Aplefen1@users.noreply.github.com>
2020-02-04 09:13:59 +0000

    Created the res of the models and made relationships between them

--- Database/Data/models.py
+++ Database/Data/models.py
@@ -2,19 +2,21 @@ from django.db import models
 from .models import *
 
 # Create your models here.
+#This is the child model
 class Child(models.Model):
-    child_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     age = models.IntegerField()
-    points = models.IntegerField()
+    points = models.IntegerField(default=0)
 
-    Classroom_id = models.ForeignKey('Classroom', on_delete=models.CASCADE)
-    Teacher_id = models.ForeignKey('Teacher', on_delete=models.CASCADE)
-    School_id = models.ForeignKey('School', on_delete=models.CASCADE)
+    #create many to one rerlationships with the other models
+    classroom = models.ForeignKey('Classroom', default=None, on_delete=models.CASCADE)
+    teacher = models.ForeignKey('Teacher', default=None, on_delete=models.CASCADE)
+    school = models.ForeignKey('School', default=None, on_delete=models.CASCADE)
 
+    #creates a nice title for the child
     def __str__(self):
         title = self.first_name + ' ' + self.last_name
         return title
@@ -22,46 +24,42 @@ class Child(models.Model):
     def Info(self):
     	return self.first_name, self.last_name, self.username, self.password, self.age, self.points
 
-    def Id(self):
-        return self.child_id
-
+#The teacher model
 class Teacher(models.Model):
-    teacher_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     username = models.CharField(max_length=32)
     password = models.CharField(max_length=32)
     age = models.IntegerField()
 
+    #Many to one relatiopnships with the relevant models
+    classroom = models.ForeignKey('Classroom', default=None, on_delete=models.CASCADE)
+    school = models.ForeignKey('School', default=None, on_delete=models.CASCADE)
+
     def __str__(self):
         title = self.first_name + ' ' + self.last_name
         return title
 
-    def Id(self):
-        return self.teacher_id
-
 class Task(models.Model):
     task_name = models.CharField(max_length=32)
-    task_id = models.AutoField(primary_key=True)
     task = models.CharField(max_length=32)
 
-    def Id(self):
-        return self.task_id
+    teacher = models.ForeignKey('Teacher', default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         title = self.task_name
         return title
 
 class Classroom(models.Model):
-    Classroom_id = models.AutoField(primary_key=True)
     classroom_name = models.CharField(max_length=32)
+
+    current_task = models.ForeignKey('Task', default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         title = self.classroom_name
         return title
 
 class School(models.Model):
-    School_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=32)
 
     def __str__(self):
